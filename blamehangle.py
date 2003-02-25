6f1b9c7869fadc7cb23a7c3666366359facafe6a
Freddie <freddie@madcowdisease.org>
2003-02-25 14:09:34 +0000

    Skeleton of a Blamehangle class
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@1356 35f662cf-58f2-0310-8820-d172d141b225

--- blamehangle.py
+++ blamehangle.py
@@ -1,3 +1,16 @@
 #!/usr/bin/env python
 
-print 'honk'
+import irclib
+
+# ---------------------------------------------------------------------------
+
+class Blamehangle:
+	"""
+	The main class. Does various exciting things, like the multiple IRC
+	server handling, and so on.
+	"""
+	
+	def __init__(self):
+		self.__ircobj = irclib.IRC()
+
+# ---------------------------------------------------------------------------
