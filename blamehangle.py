a49c2015fdfd35a61fb7b0995f49cd0643ea2232
Freddie <freddie@madcowdisease.org>
2003-02-25 15:11:43 +0000

    Add config file loading
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@1362 35f662cf-58f2-0310-8820-d172d141b225

--- blamehangle.py
+++ blamehangle.py
@@ -1,13 +1,47 @@
 #!/usr/bin/env python
 
+import getopt
+import os
 import sys
+
+from ConfigParser import ConfigParser
 
 # ---------------------------------------------------------------------------
 
 def main():
-	"The initial setup, config reading, and so on."
+	"The initial setup, config reading, and probably main loop."
 	
-	print 'honk!'
+	# Parse our command line options
+	try:
+		opts, args = getopt.getopt(sys.argv[1:], "bc:", [ "background", "config=" ])
+	
+	except getopt.GetoptError:
+		Show_Usage()
+	
+	ConfigFile = None
+	for opt, arg in opts:
+		if opt in ('-c', '--config'):
+			ConfigFile = arg
+	
+	# Load our config file
+	if not ConfigFile or not os.path.exists(ConfigFile):
+		Show_Usage()
+	
+	Config = ConfigParser()
+	Config.read(ConfigFile)
+	
+	print Config.sections()
+
+# ---------------------------------------------------------------------------
+	
+def Show_Usage():
+	print "USAGE: %s [OPTIONS]" % argv[0]
+	print
+	print " -b, --background    run in the background (no screen output)(doesn't work)"
+	print " -c, --config=FILE   config file to use"
+	print
+	
+	sys.exit(-1)
 
 # ---------------------------------------------------------------------------
 
