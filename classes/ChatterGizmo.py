08922ae0ab744a804effa835c7234ccb09846533
Freddie <freddie@madcowdisease.org>
2003-03-05 04:21:31 +0000

    Added some more stuff
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@1391 35f662cf-58f2-0310-8820-d172d141b225

--- classes/ChatterGizmo.py
+++ classes/ChatterGizmo.py
@@ -1,4 +1,7 @@
 
+import errno
+import select
+
 from classes import irclib
 
 # ---------------------------------------------------------------------------
@@ -17,4 +20,31 @@ class ChatterGizmo:
 		self.Conns = {}
 	
 	def main_loop(self):
-		print 'main!'
+		try:
+			self.__ircobj.process_once()
+		
+		except select.error, msg:
+			if msg[0] == errno.EINTR:
+				pass
+	
+	# -----------------------------------------------------------------------
+	
+	def _handle_welcome(self, conn, event):
+		#tolog = 'Connected to %s' % self.server_list[0]
+		#self.putlog(LOG_ALWAYS, tolog)
+		
+		#self.status = STATUS_CONNECTED
+		#self.__Connect_Last = time.time()
+		
+		#self.stoned = 0
+		
+		# Initialise the channels dictionary
+		#self.__Users = UserList()
+		
+		# Start the stoned timer thing
+		#self.__Stoned_Check()
+		
+		# Tell FileMonster what our local IP is
+		#self.sendMessage('FileMonster', REPLY_LOCALADDR, self.connection.socket.getsockname())
+		
+		print 'Welcome to Floston Paradise!'
