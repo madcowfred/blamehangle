cab34da99e27b88ea3f078b8de94ab0e27d6dc3c
Freddie <freddie@madcowdisease.org>
2003-03-13 07:07:33 +0000

    Some non-blocking magic
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@1521 35f662cf-58f2-0310-8820-d172d141b225

--- classes/HTTPMonster.py
+++ classes/HTTPMonster.py
@@ -9,14 +9,17 @@
 # This is done so that url requests will not cause the bot to hang if the
 # remote http server responds slowly.
 
-from urllib2 import *
 from Queue import Queue
+from select import select
 from thread import start_new_thread
-from threading import BoundedSemaphore
+from time import sleep
+from urllib2 import urlopen
 
 from classes.Children import Child
 from classes.Constants import *
 from classes.Common import *
+
+# ---------------------------------------------------------------------------
 
 class HTTPMonster(Child):
 	"""
@@ -24,12 +27,9 @@ class HTTPMonster(Child):
 	This class takes requests for URLs and fetches them in a new thread
 	to ensure that the bot will not freeze due to slow servers, or whatever
 	"""
-
-	def setup(self):
-		self.Sem = BoundedSemaphore()
 	
 	# -----------------------------------------------------------------------
-
+	
 	def _message_REQ_URL(self, message):
 		start_new_thread(URLThread, (self, message))
 
@@ -40,18 +40,26 @@ def URLThread(parent, message):
 	try:
 		# get the page
 		the_page = urlopen(url)
-		pagetext = the_page.read()
+		
+		pagetext = ''
+		while 1:
+			can_read = select([the_page], [], [], 1)[0]
+			if can_read:
+				data = the_page.read(1024)
+				if len(data) == 0:
+					break
+				
+				pagetext += data
+				
+				sleep(0.05)
+	
 	except Exception, why:
 		# something borked
-		parent.Sem.acquire()
 		tolog = "Error while trying to fetch url: %s - %s" % (url, why)
 		parent.putlog(LOG_ALWAYS, tolog)
-		parent.Sem.release()
+	
 	else:
 		# we have the page
 		data = [pagetext, returnme]
 		message = Message('HTTPMonster', message.source, REPLY_URL, data)
-		parent.Sem.acquire()
 		parent.outQueue.put(message)
-		parent.Sem.release()
-	
