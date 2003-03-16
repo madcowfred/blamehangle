d965a56088593b92287b8c92c7cfe77bda702354
rejj <rejj@automagic.org>
2003-03-16 16:40:00 +0000

    added "blah is also blahblah"
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@1562 35f662cf-58f2-0310-8820-d172d141b225

--- plugins/SmartyPants.py
+++ plugins/SmartyPants.py
@@ -41,6 +41,7 @@ from classes.Constants import *
 
 FACT_SET = "FACT_SET"
 FACT_GET = "FACT_GET"
+FACT_ALSO = "FACT_ALSO"
 FACT_DEL = "FACT_DEL"
 FACT_MOD = "FACT_REPLACE"
 FACT_INFO = "FACT_INFO"
@@ -51,13 +52,15 @@ FACT_UPDATEDB = "FACT_MOD"
 
 GET_QUERY = "SELECT name, value FROM factoids WHERE name = %s"
 SET_QUERY = "INSERT INTO factoids (name, value, author_nick, author_host, created_time) VALUES (%s, %s, %s, %s, %s)"
+MOD_QUERY = "UPDATE factoids SET value = %s, modifier_nick = %s, modifier_host = %s, modified_time = %s WHERE name = %s"
 DEL_QUERY = "DELETE FROM factoids WHERE name = %s"
 INFO_QUERY = "SELECT * FROM factoids WHERE name = %s"
 
 REQUESTED_QUERY = "UPDATE factoids SET request_count = request_count + 1, requester_nick = %s, requester_host = %s WHERE name = %s"
 
 GET_RE = re.compile("^(?P<name>.+?)\??$")
-SET_RE = re.compile("^(?P<name>.+?) (is|are) (?P<value>.+)$")
+SET_RE = re.compile("^(?P<name>.+?) (is|are) (?!also )(?P<value>.+)$")
+ALSO_RE = re.compile("(?P<name>.+?) (is|are) also (?P<value>.+)$")
 DEL_RE = re.compile("^forget (?P<name>.+)$")
 MOD_RE = re.compile("^(?P<name>.+?) =~ (?P<modstring>.+)$")
 INFO_RE = re.compile("^factinfo (?P<name>.+)\??$")
@@ -102,6 +105,8 @@ class SmartyPants(Plugin):
 		get_msg = PluginTextEvent(FACT_GET, IRCT_MSG, GET_RE, exclusive=1)
 		set_dir = PluginTextEvent(FACT_SET, IRCT_PUBLIC_D, SET_RE)
 		set_msg = PluginTextEvent(FACT_SET, IRCT_MSG, SET_RE)
+		also_dir = PluginTextEvent(FACT_ALSO, IRCT_PUBLIC_D, ALSO_RE)
+		also_msg = PluginTextEvent(FACT_ALSO, IRCT_MSG, ALSO_RE)
 		del_dir = PluginTextEvent(FACT_DEL, IRCT_PUBLIC_D, DEL_RE)
 		del_msg = PluginTextEvent(FACT_DEL, IRCT_MSG, DEL_RE)
 		#mod_dir =
@@ -109,7 +114,7 @@ class SmartyPants(Plugin):
 		info_dir = PluginTextEvent(FACT_INFO, IRCT_PUBLIC_D, INFO_RE)
 		info_msg = PluginTextEvent(FACT_INFO, IRCT_MSG, INFO_RE)
 		
-		self.register(get_dir, get_msg, set_dir, set_msg, del_dir, del_msg, info_dir, info_msg)
+		self.register(get_dir, get_msg, set_dir, set_msg, also_dir, also_msg, del_dir, del_msg, info_dir, info_msg)
 	
 	#------------------------------------------------------------------------
 	
@@ -126,7 +131,17 @@ class SmartyPants(Plugin):
 		# to go to hell.
 		elif trigger.name == FACT_SET:
 			name = trigger.match.group('name')
-			if len(name) > 63:
+			if len(name) > 32:
+				replytext = "factoid name is too long"
+				self.sendReply(trigger, replytext)
+			else:
+				data = [trigger, (GET_QUERY, [name])]
+				self.sendMessage('DataMonkey', REQ_QUERY, data)
+
+		# Someone wants to add to the definition of a factoid
+		elif trigger.name == FACT_ALSO:
+			name = trigger.match.group('name')
+			if len(name) > 32:
 				replytext = "factoid name is too long"
 				self.sendReply(trigger, replytext)
 			else:
@@ -155,6 +170,9 @@ class SmartyPants(Plugin):
 		
 		elif trigger.name == FACT_SET:
 			self.__Fact_Set(trigger, results)
+
+		elif trigger.name == FACT_ALSO:
+			self.__Fact_Also(trigger, results)
 		
 		elif trigger.name == FACT_DEL:
 			self.__Fact_Del(trigger, results)
@@ -241,7 +259,52 @@ class SmartyPants(Plugin):
 			elif result == 1:
 				replytext = self.__Random(OK)
 			self.sendReply(trigger, replytext)
-	
+
+	# -----------------------------------------------------------------------
+	# A user just tried to update a factoid. If it exists, we add to its
+	# definition, otherwise we make a new factoid.
+	# -----------------------------------------------------------------------
+	def __Fact_Also(self, trigger, results):
+		typ = type(results[0])
+
+		# SELECT reply
+		if typ == types.TupleType:
+			if results == [()]:
+				# The factoid wasn't in our database, so insert it
+				# We cheat a little here
+				trigger.name = FACT_SET
+				self.__Fact_Set(trigger, results)
+
+			else:
+				# It was already in our database, so add to the definition
+				# if it fits
+				row = results[0][0]
+				old_value = row['value']
+				new_value = "%s, or %s" % (old_value, trigger.match.group('value'))
+				# I stole this number from devinfo's config file.
+				# we should probably put this into a config file also,
+				# but then again maybe not. At least this way people can't
+				# break things. heh.
+				if len(new_value) > 455:
+					replytext = "that's too long"
+					self.sendReply(trigger, replytext)
+				else:
+					modified_time = time.time()
+					modifier_nick = trigger.userinfo.nick
+					modifier_host = "%s@%s" % (trigger.userinfo.ident, trigger.userinfo.host)
+					name = trigger.match.group('name')
+					data = [trigger, (MOD_QUERY, [new_value, modifier_nick, modifier_host, modified_time, name])]
+					self.sendMessage('DataMonkey', REQ_QUERY, data)
+					
+		# UPDATE reply
+		elif typ == types.LongType:
+			result = results[0]
+			if result == 0:
+				replytext = 'factoid insertion failed, warning, warning!'
+			elif result == 1:
+				replytext = self.__Random(OK)
+			self.sendReply(trigger, replytext)
+
 	# -----------------------------------------------------------------------
 	# Someone asked to delete a factoid.
 	#
