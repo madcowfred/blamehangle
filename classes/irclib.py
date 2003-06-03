ffccbf8135a3eef2002fd69a2d2fa30835c14a56
Freddie <freddie@madcowdisease.org>
2003-06-03 08:54:03 +0000

    Blarg
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@1796 35f662cf-58f2-0310-8820-d172d141b225

--- classes/irclib.py
+++ classes/irclib.py
@@ -408,8 +408,7 @@ class ServerConnection(Connection):
 		self.password = password
 		self.localhost = socket.gethostname()
 		
-		if vhost is not None:
-			print 'argh'
+		if vhost:
 			try:
 				res = socket.getaddrinfo(vhost, 0, socket.AF_UNSPEC, socket.SOCK_STREAM)[0]
 				af, socktype, proto, canonname, sa = res
