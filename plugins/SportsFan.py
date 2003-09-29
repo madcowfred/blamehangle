29a2c14a727826b18c37426f8cfc06453cab84f1
Freddie <freddie@madcowdisease.org>
2003-09-29 14:29:37 +0000

    Fix the broken parsing
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@2022 35f662cf-58f2-0310-8820-d172d141b225

--- plugins/SportsFan.py
+++ plugins/SportsFan.py
@@ -3,19 +3,17 @@
 # ---------------------------------------------------------------------------
 # Yahoo Sports game result lookup
 
+import re
+
 from classes.Plugin import *
 from classes.Constants import *
 
-import re, cStringIO
+# ---------------------------------------------------------------------------
 
 SCORES = "SCORES"
+SCORES_RE = re.compile("^score\s+(?P<league>NFL|MLB|NBA|NHL)\s+(?P<team>.+)$", re.I)
 
-s0 = "^score +"
-s1 = "(?P<league>(NFL|nfl|MLB|mlb|NBA|nba|NHL|nhl)) +"
-s2 = "(?P<team>.+)$"
-
-SCORES_RE = re.compile(s0+s1+s2)
-
+# ---------------------------------------------------------------------------
 
 class SportsFan(Plugin):
 	"""
@@ -23,26 +21,26 @@ class SportsFan(Plugin):
 	league can be one of: NFL, MLB, NBA, NHL
 	team is identified by location, not nickname
 	"""
-
+	
 	# -----------------------------------------------------------------------
-
+	
 	def _message_PLUGIN_REGISTER(self, message):
 		scores_dir = PluginTextEvent(SCORES, IRCT_PUBLIC_D, SCORES_RE)
 		scores_msg = PluginTextEvent(SCORES, IRCT_MSG, SCORES_RE)
-
+		
 		self.register(scores_dir, scores_msg)
 		self.__set_help_msgs()
 	
 	def __set_help_msgs(self):
 		SCORES_HELP = "'\02score\02 <league> <team>' : Search for a score for <team> playing in <league> today. <league> can be one of NFL, MLB, NBA, NHL. <team> is identified by location, not nickname."
-
+		
 		self.setHelp('sports', 'score', SCORES_HELP)
 	
 	# -----------------------------------------------------------------------
-
+	
 	def _message_PLUGIN_TRIGGER(self, message):
 		trigger = message.data
-
+		
 		if trigger.name == SCORES:
 			self.__scores(trigger)
 		else:
@@ -50,83 +48,58 @@ class SportsFan(Plugin):
 			raise ValueError, errtext
 	
 	# -----------------------------------------------------------------------
-
+	
 	# Someone wants us to lookup a sports score
 	def __scores(self, trigger):
-		league = trigger.match.group('league').lower()
-		url = "http://sports.yahoo.com/" + league + "/"
-
+		url = "http://sports.yahoo.com/%s/" % trigger.match.group('league').lower()
 		self.urlRequest(trigger, url)
 	
 	# -----------------------------------------------------------------------
-
+	
 	# We heard back from Yahoo. yay!
 	def _message_REPLY_URL(self, message):
 		trigger, page_text = message.data
 		team = trigger.match.group('team')
 		league = trigger.match.group('league')
-
-		# Search for our info in the page Yahoo Sports gave us
-		s = cStringIO.StringIO(page_text)
 		
-		line = s.readline()
-		while line:
-			line = line.strip()
-			if line.startswith('<a href="/%s/teams/' % league.lower()) \
-			and line.endswith("at<br>"):
+		# Search for our info in the page Yahoo Sports gave us
+		find_re = re.compile(r'^\s*<a href="/%s/teams/.*?at<br>$' % league, re.I)
+		
+		lines = page_text.splitlines()
+		for i in range(len(lines)):
+			line = lines[i]
+			if find_re.match(line):
 				# We found the away team in this game
 				away = self.__parse(line)[:-3]
 				
 				# the next line is the home team
-				line = s.readline()
-				home = self.__parse(line)
-
-				# there are two lines with only html in them that follow
-				# yahoo have changed their html, only one line now
-				# line = s.readline()
-				line = s.readline()
-
-				# the next line has the scores
-				line = s.readline()
-				away_score, home_score = self.__find_scores(line)
-
-				# the next 3 lines contain nothing interesting
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-
-				# the next line indicates if this is a final score or not
-				line = s.readline()
-				line = self.__parse(line)
-				if line == "Final":
+				home = self.__parse(lines[i+1])
+				
+				# one line of crappy html, then the scores
+				away_score, home_score = self.__find_scores(lines[i+3])
+				
+				# this line indicates if this is a final score or not
+				blah = self.__parse(lines[i+7])
+				if blah == "Final":
 					game_status = "(Final score)"
 				else:
 					game_status = "(Progress score)"
-
+				
 				tolog = "Found a game: %s %s: %s - %s: %s" % (game_status, home, home_score, away, away_score)
 				self.putlog(LOG_DEBUG, tolog)
-
+				
 				# check to see if this is the game the user was asking about
 				if team.lower() == away.lower() or team.lower() == home.lower():
-					replytext = "%s " % game_status
-					replytext += "%s: %s - " % (home, home_score)
-					replytext += "%s: %s" % (away, away_score)
+					replytext = '%s %s: %s - %s: %s' % (game_status, home, home_score, away, away_score)
 					self.sendReply(trigger, replytext)
-					s.close()
 					return
-			
-			# Keep looping, looking for games
-			line = s.readline()
-
+		
 		# If we get here, we didn't find any games that matched what the user
 		# was looking for
 		replytext = "Couldn't find a %s game today for '%s'" % (league, team)
 		self.sendReply(trigger, replytext)
-		s.close()
-
-
-	# -----------------------------------------------------------------------
 	
+	# -----------------------------------------------------------------------
 	# remove all the HTML tags and the trailing newline from the supplied line
 	def __parse(self, text):
 		text = re.sub("<.+?>", "", text)
@@ -134,9 +107,8 @@ class SportsFan(Plugin):
 		if text.endswith("\n"):
 			text = text[:-1]
 		return text.strip()
-
+	
 	# -----------------------------------------------------------------------
-
 	# Find the scores in the current line
 	def __find_scores(self, text):
 		text = text.replace("<", " <")
