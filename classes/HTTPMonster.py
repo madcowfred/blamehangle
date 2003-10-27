89469c18da927824ae82865d7dd5634ac8e2f24e
Freddie <freddie@madcowdisease.org>
2003-10-27 19:14:55 +0000

    Debut of the new HTTP Monster!
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@2137 35f662cf-58f2-0310-8820-d172d141b225

--- classes/HTTPMonster.py
+++ classes/HTTPMonster.py
@@ -9,14 +9,18 @@
 # This is done so that url requests will not cause the bot to hang if the
 # remote http server responds slowly.
 
-import select
+import asyncore
+#import select
+import socket
+import sys
 import time
+import urlparse
 
-from Queue import *
+#from Queue import *
 #from thread import start_new_thread
-from threading import *
+#from threading import *
 # we have our own version which doesn't mangle our User-Agent
-from classes import urllib2
+#from classes import urllib2
 
 from classes.Children import Child
 from classes.Constants import *
@@ -36,30 +40,32 @@ class HTTPMonster(Child):
 	"""
 	
 	def setup(self):
+		self.rehash()
+		
+		self.urls = []
+		#self.threads = []
+	
+	def rehash(self):
+		# Set up our connection limit
+		if self.Config.has_option('HTTP', 'connections'):
+			self.max_conns = min(1, max(10, self.Config.getint('HTTP', 'connections')))
+		else:
+			self.max_conns = 1
+		
+		# Set up our user-agent
 		if self.Config.has_option('HTTP', 'useragent'):
 			self.user_agent = self.Config.get('HTTP', 'useragent')
 		else:
-			# Default to FireBird
-			self.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.4b) Gecko/20030516 Mozilla Firebird/0.6"
+			self.user_agent = "Mozilla/5.0 (Windows; U; Windows NT 5.0; en-US; rv:1.5) Gecko/20031015 Firebird/0.7"
 		
-		# Set up our threads
-		if self.Config.has_option('HTTP', 'connections'):
-			self.conns = min(1, max(10, self.Config.getint('HTTP', 'connections')))
-		else:
-			self.conns = 1
-		
-		self.urls = Queue(0)
-		self.threads = []
+		#self.__stop_threads()
+		#self.setup()
+		#self.run_once()
 	
-	def rehash(self):
-		self.__stop_threads()
-		self.setup()
-		self.run_once()
-	
-	def shutdown(self, message):
+	def noshutdown(self, message):
 		self.__stop_threads()
 	
-	def run_once(self):
+	def norun_once(self):
 		for i in range(self.conns):
 			the_thread = Thread(target=URLThread, args=(self,i))
 			self.threads.append([the_thread,0])
@@ -84,22 +90,133 @@ class HTTPMonster(Child):
 	
 	# -----------------------------------------------------------------------
 	
+	def run_always(self):
+		asyncore.poll()
+		
+		if self.urls and len(asyncore.socket_map) < self.max_conns:
+			async_http(self, self.urls.pop(0))
+
+	# -----------------------------------------------------------------------
+	
 	def _message_REQ_URL(self, message):
-		self.urls.put(message)
+		if len(asyncore.socket_map) < self.max_conns:
+			async_http(self, message)
+		else:
+			self.urls.append(message)
+		
 		#start_new_thread(URLThread, (self, message))
 
 # ---------------------------------------------------------------------------
 
-# Will work on this one day (fred)
-#class URLThread(threading.Thread):
-#	def __init__(self, parent):
-#		threading.Thread.__init__(self)
-#		
-#		self.parent = parent
-#		self.stopnow = 0
-#	
-#	def run(self):
+class async_http(asyncore.dispatcher_with_send):
+	def __init__(self, parent, message):
+		asyncore.dispatcher_with_send.__init__(self)
+		
+		self.data = ''
+		self.header = ''
+		
+		self.parent = parent
+		self.source = message.source
+		self.returnme = message.data[0]
+		self.url = message.data[1]
+		
+		# Log what we're doing
+		tolog = 'Fetching URL: %s' % (self.url)
+		parent.putlog(LOG_DEBUG, tolog)
+		
+		# Parse the URL, saving the bits we need
+		scheme, host, path, params, query, fragment = urlparse.urlparse(self.url)
+		
+		# Work out our host/port from the host field
+		try:
+			host, port = host.split(":", 1)
+			port = int(port)
+		except (TypeError, ValueError):
+			port = 80
+		
+		self.host = host
+		
+		# Fix up the path field
+		if not path:
+			path = '/'
+		if params:
+			path = '%s;%s' % (path, params)
+		if query:
+			path = '%s?%s' % (path, query)
+		
+		self.path = path
+		
+		# Create the socket and start the connection
+		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
+		self.connect((host, port))
+	
+	# Connection succeeded
+	def handle_connect(self):
+		text = "GET %s HTTP/1.1\r\n" % (self.path)
+		self.send(text)
+		text = "Host: %s\r\n" % (self.host)
+		self.send(text)
+		text = "User-Agent: %s\r\n\r\n" % (self.parent.user_agent)
+		self.send(text)
+	
+	# An exception occured somewhere
+	def handle_error(self):
+		t, v, tb = sys.exc_info()
+		del tb
+		
+		if t == 'KeyboardInterrupt':
+			raise
+		else:
+			tolog = "Error while trying to fetch url: %s - %s" % (self.url, v)
+			self.parent.putlog(LOG_ALWAYS, tolog)
+		
+		self.close()
+	
+	# Connection has data to read
+	def handle_read(self):
+		data = self.recv(512)
+		self.data += data
+		
+		if not self.header:
+			chunks = self.data.split('\r\n\r\n', 1)
+			if len(chunks) <= 1:
+				return
+			
+			header, data = chunks
+			self.header = header
+			self.data = data
+			
+			#print self.header.splitlines()
+	
+	# Connection has been closed
+	def handle_close(self):
+		#if self.data:
+		# We have some data, might as well process it?
+		self.process_data(self.data)
+	
+	def process_data(self, pagetext):
+		if len(pagetext) > 0:
+			# Dodgy HTML fix up time
+			m = dodgy_html_check(pagetext)
+			while m:
+				pre = pagetext[:m.start()]
+				post = pagetext[m.end():]
+				start, end = m.span('href')
+				fixed = '"' + pagetext[start:end - 1].replace("'", "%39") + '"'
+				pagetext = pre + 'href=' + fixed + post
+				m = dodgy_html_check(pagetext)
+			
+			tolog = 'Finished fetching URL: %s - %d bytes' % (self.url, len(pagetext))
+			self.parent.putlog(LOG_DEBUG, tolog)
+			
+			data = [self.returnme, pagetext]
+			#message = Message('HTTPMonster', self.source, REPLY_URL, data)
+			#self.parent.outQueue.append(message)
+			self.parent.sendMessage(self.source, REPLY_URL, data)
+		
+		self.close()
 
+# ---------------------------------------------------------------------------
 
 def URLThread(parent, myindex):
 	_select = select.select
