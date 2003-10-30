1b68dcbe59041ce34b64eb1d04fee90783c06d83
Freddie <freddie@madcowdisease.org>
2003-10-30 00:25:41 +0000

    Add a 'quote' command for stock quotes
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@2161 35f662cf-58f2-0310-8820-d172d141b225

--- plugins/MoneyMangler.py
+++ plugins/MoneyMangler.py
@@ -7,18 +7,25 @@ import os
 import re
 from HTMLParser import HTMLParser
 
+from classes.Common import *
 from classes.Constants import *
 from classes.Plugin import *
 
 # ---------------------------------------------------------------------------
 
 MONEY_CURRENCY = 'MONEY_CURRENCY'
-MONEY_EXCHANGE = 'MONEY_EXCHANGE'
-
 CURRENCY_RE = re.compile('^currency (?P<curr>\w+)$')
-EXCHANGE_RE = re.compile('^exchange (?P<amt>[\d\.]+) (?P<from>\w\w\w)(?: to | )(?P<to>\w\w\w)$')
+CURRENCY_HELP = "'\02currency\02 <code OR partial name>' : Look up the currency code and name, given the specified information"
 
+MONEY_EXCHANGE = 'MONEY_EXCHANGE'
+EXCHANGE_RE = re.compile('^exchange (?P<amt>[\d\.]+) (?P<from>\w\w\w)(?: to | )(?P<to>\w\w\w)$')
+EXCHANGE_HELP = "'\02exchange\02 <amount> <currency 1> \02to\02 <currency 2>' : Convert currency using current exchange rates. Currencies are specified using their three letter code."
 EXCHANGE_URL = 'http://finance.yahoo.com/m5?a=%(amt)s&s=%(from)s&t=%(to)s&c=0'
+
+MONEY_QUOTE = 'MONEY_QUOTE'
+QUOTE_RE = re.compile('^quote (?P<symbol>\S+)$')
+QUOTE_HELP = '\02quote\02 <symbol> : Look up a current stock price.'
+QUOTE_URL = 'http://finance.yahoo.com/q?d=v1&s=%s'
 
 # ---------------------------------------------------------------------------
 
@@ -57,21 +64,19 @@ class MoneyMangler(Plugin):
 		curr_msg = PluginTextEvent(MONEY_CURRENCY, IRCT_MSG, CURRENCY_RE)
 		conv_dir = PluginTextEvent(MONEY_EXCHANGE, IRCT_PUBLIC_D, EXCHANGE_RE)
 		conv_msg = PluginTextEvent(MONEY_EXCHANGE, IRCT_MSG, EXCHANGE_RE)
+		quote_dir = PluginTextEvent(MONEY_QUOTE, IRCT_PUBLIC_D, QUOTE_RE)
+		quote_msg = PluginTextEvent(MONEY_QUOTE, IRCT_MSG, QUOTE_RE)
 		
-		self.register(conv_dir, conv_msg, curr_dir, curr_msg)
-		self.__set_help_msgs()
-	
-	def __set_help_msgs(self):
-		MONEY_CURRENCY_HELP = "'\02currency\02 <code OR partial name>' : Look up the currency code and name, given the specified information"
-		MONEY_EXCHANGE_HELP = "'\02exchange\02 <amount> <currency 1> \02to\02 <currency 2>' : Convert currency using current exchange rates. Currencies are specified using their three letter code."
+		self.register(conv_dir, conv_msg, curr_dir, curr_msg, quote_dir, quote_msg)
 		
-		self.setHelp('money', 'currency', MONEY_CURRENCY_HELP)
-		self.setHelp('money', 'exchange', MONEY_EXCHANGE_HELP)
+		self.setHelp('money', 'currency', CURRENCY_HELP)
+		self.setHelp('money', 'exchange', EXCHANGE_HELP)
+		self.setHelp('money', 'quote', QUOTE_HELP)
 		
 		self.registerHelp()
 	
 	# -----------------------------------------------------------------------
-
+	
 	def _message_PLUGIN_TRIGGER(self, message):
 		trigger = message.data
 		
@@ -82,12 +87,19 @@ class MoneyMangler(Plugin):
 		# Someone wants to do a money conversion
 		elif trigger.name == MONEY_EXCHANGE:
 			self.__Currency_Exchange(trigger)
+		
+		# Someone wants to look up a stock price
+		elif trigger.name == MONEY_QUOTE:
+			self.__Stock_Quote(trigger)
+	
+	# -----------------------------------------------------------------------
 	
 	def _message_REPLY_URL(self, message):
 		(trigger, data), page_text = message.data
-		page_text = page_text.replace('&amp;', ' and ')
 		
 		if trigger.name == MONEY_EXCHANGE:
+			page_text = page_text.replace('&amp;', ' and ')
+			
 			parser = YahooParser()
 			
 			try:
@@ -97,19 +109,59 @@ class MoneyMangler(Plugin):
 				replytext = "Error parsing the html"
 				self.sendReply(trigger, replytext)
 			else:
-			
 				#currencies = {}
 				#for curr in parser.currs:
 				#	name = curr[:-6]
 				#	code = curr[-4:-1]
 				#	currencies[code] = name
 				#cPickle.dump(currencies, open('configs/currency.data', 'wb'), 1)
-			
+				
 				if parser.result:
 					replytext = '%s %s == %s %s' % (data['amt'], data['from'], parser.result, data['to'])
 				else:
 					replytext = 'No result returned.'
 				self.sendReply(trigger, replytext)
+		
+		elif trigger.name == MONEY_QUOTE:
+			# Invalid symbol, sorry
+			if page_text.find('is not a valid ticker symbol') >= 0:
+				replytext = '"%s" is not a valid ticker symbol!' % data
+			
+			else:
+				# Find the data we need
+				chunk = FindChunk(page_text, '<table class="yfnc_datamodoutline1"', '</table>')
+				if chunk is None:
+					self.putlog(LOG_WARNING, 'Stock page parsing failed: no stock data')
+					self.sendReply(trigger, 'Failed to parse page properly')
+					return
+				
+				# Replace the up/down graphics with +/-
+				chunk = re.sub(r'alt="Down">\s*', '>-', chunk)
+				chunk = re.sub(r'alt="Up">\s*', '>+', chunk)
+				
+				# Strip the evil HTML!
+				lines = StripHTML(chunk)
+				
+				# Sort out the stock info
+				info = {'Symbol': data}
+				for line in lines:
+					parts = re.split(r'\s*:\s*', line, 1)
+					if len(parts) == 2 and parts[1] != 'N/A':
+						if parts[0] in ('Last Trade', 'Index Value'):
+							info['Value'] = parts[1]
+						else:
+							info[parts[0]] = parts[1]
+				
+				# Output something now :)
+				if info:
+					try:
+						replytext = '[%(Trade Time)s] %(Symbol)s: %(Value)s %(Change)s' % info
+					except KeyError:
+						replytext = 'Some stock data missing, not good!'
+				else:
+					replytext = 'No stock data found? WTF?'
+			
+			self.sendReply(trigger, replytext)
 	
 	# -----------------------------------------------------------------------
 	
@@ -150,8 +202,6 @@ class MoneyMangler(Plugin):
 			if name.lower().find(curr) >= 0:
 				found.append(code)
 		
-		#mmhmeiu
-		
 		replytext = "Currency search for '%s'" % curr
 		
 		if found:
@@ -166,6 +216,14 @@ class MoneyMangler(Plugin):
 			replytext += ': No matches found.'
 		
 		self.sendReply(trigger, replytext)
+	
+	# -----------------------------------------------------------------------
+	
+	def __Stock_Quote(self, trigger):
+		symbol = trigger.match.group('symbol').upper()
+		returnme = (trigger, symbol)
+		fetchme = QUOTE_URL % symbol
+		self.urlRequest(returnme, fetchme)
 
 # ---------------------------------------------------------------------------
 # A parser for the Yahoo Finance currency conversion page
