19ec242f80c03431ca3467165fcf9114ae164026
Freddie <freddie@madcowdisease.org>
2003-10-30 01:47:50 +0000

    Update the comments
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@2172 35f662cf-58f2-0310-8820-d172d141b225

--- classes/HTTPMonster.py
+++ classes/HTTPMonster.py
@@ -3,8 +3,8 @@
 # ---------------------------------------------------------------------------
 # This file implements the url retriever for blamehangle. Plugins can send
 # a message to the class defined here asking for the contents of a page
-# defined by the given url, and a new thread will be spawned to go and fetch
-# the data.
+# defined by the given url, and a new dispatcher will be spawned eventually
+# to fetch the URL.
 #
 # This is done so that url requests will not cause the bot to hang if the
 # remote http server responds slowly.
