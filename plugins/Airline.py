d86d152252638902326dc3577a3735b0eaed8aac
Freddie <freddie@madcowdisease.org>
2003-10-31 22:44:59 +0000

    Rewrite the page parsing code to use FindChunk/StripHTML, various other cleanups
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@2197 35f662cf-58f2-0310-8820-d172d141b225

--- plugins/Airline.py
+++ plugins/Airline.py
@@ -5,18 +5,25 @@
 # You can lookup an airline's code, or get details on a flight number.
 # ---------------------------------------------------------------------------
 
-from classes.Plugin import *
+import re
+import time
+import types
+import cPickle
+
+from classes.Common import *
 from classes.Constants import *
-from cStringIO import *
-import re, time, types, cPickle
+from classes.Plugin import *
 
-AIRLINE_LOOKUP = "AIRLINE_LOOKUP"
-FLIGHT_SEARCH = "FLIGHT_SEARCH"
-
-AIRLINE_RE = re.compile("^ *airline +(?P<airline>.+)$")
+# ---------------------------------------------------------------------------
 
 MAX_AIRLINE_MATCHES = 5
 
+AIRLINE_AIRLINE = 'AIRLINE_AIRLINE'
+AIRLINE_HELP = "'\02airline\02 <code>' OR '\02airline\02 <partial name>' : lookup the name for a carrier given the code, or lookup the code  for a carrier given the name (or start of the name)"
+AIRLINE_RE = re.compile("^airline +(?P<airline>.+)$")
+
+AIRLINE_FLIGHT = 'AIRLINE_FLIGHT'
+FLIGHT_HELP = "'\02flight\02 <code> <flight number> <date>' : Lookup the details of the specified flight. date is in YYYY-MM-DD format, and is optional (defaults to today's date if ommitted)"
 # what a bastard this was to get right. god damn i hate regexps.
 f1 = "^ *flight +"
 f2 = "(?P<code>[^ ]+)"
@@ -30,6 +37,7 @@ f9 = f4 % f8
 
 FLIGHT_RE = re.compile(f1+f2+f3+f9)
 
+# ---------------------------------------------------------------------------
 
 class Airline(Plugin):
 	"""
@@ -38,7 +46,7 @@ class Airline(Plugin):
 	and it was much later when i finished.
 	comments come later
 	"""
-
+	
 	def setup(self):
 		config_dir = self.Config.get('plugin', 'config_dir')
 		self.Airlines = {}
@@ -64,17 +72,12 @@ class Airline(Plugin):
 	# --------------------------------------------------------------------------
 	
 	def _message_PLUGIN_REGISTER(self, message):
-		air_dir = PluginTextEvent(AIRLINE_LOOKUP, IRCT_PUBLIC_D, AIRLINE_RE)
-		air_msg = PluginTextEvent(AIRLINE_LOOKUP, IRCT_MSG, AIRLINE_RE)
-		fl_dir = PluginTextEvent(FLIGHT_SEARCH, IRCT_PUBLIC_D, FLIGHT_RE)
-		fl_msg = PluginTextEvent(FLIGHT_SEARCH, IRCT_MSG, FLIGHT_RE)
-
+		air_dir = PluginTextEvent(AIRLINE_AIRLINE, IRCT_PUBLIC_D, AIRLINE_RE)
+		air_msg = PluginTextEvent(AIRLINE_AIRLINE, IRCT_MSG, AIRLINE_RE)
+		fl_dir = PluginTextEvent(AIRLINE_FLIGHT, IRCT_PUBLIC_D, FLIGHT_RE)
+		fl_msg = PluginTextEvent(AIRLINE_FLIGHT, IRCT_MSG, FLIGHT_RE)
+		
 		self.register(air_dir, air_msg, fl_dir, fl_msg)
-		self.__set_help_msgs()
-	
-	def __set_help_msgs(self):
-		AIRLINE_HELP = "'\02airline\02 <code>' OR '\02airline\02 <partial name>' : lookup the name for a carrier given the code, or lookup the code  for a carrier given the name (or start of the name)"
-		FLIGHT_HELP = "'\02flight\02 <code> <flight number> <date>' : Lookup the details of the specified flight. date is in YYYY-MM-DD format, and is optional (defaults to today's date if ommitted)"
 		
 		self.setHelp('travel', 'airline', AIRLINE_HELP)
 		self.setHelp('travel', 'flight', FLIGHT_HELP)
@@ -86,13 +89,12 @@ class Airline(Plugin):
 	def _message_PLUGIN_TRIGGER(self, message):
 		trigger = message.data
 		
-		if trigger.name == AIRLINE_LOOKUP:
+		if trigger.name == AIRLINE_AIRLINE:
 			self.__airline_lookup(trigger)
-		elif trigger.name == FLIGHT_SEARCH:
+		elif trigger.name == AIRLINE_FLIGHT:
 			self.__flight_search(trigger)
 	
 	# --------------------------------------------------------------------------
-	
 	# Someone wants to lookup an airline. If they gave us the code, we'll find
 	# the name string, if they gave us a name string, we'll try to find codes
 	# that match it
@@ -132,20 +134,20 @@ class Airline(Plugin):
 					matches.append((code, self.Airlines[code]))
 
 			return matches
-
-# ---------------------------------------------------------------------------
-
+	
+	# -----------------------------------------------------------------------
+	
 	# Someone wants to look up info on a flight.
 	def __flight_search(self, trigger):
 		code = trigger.match.group('code').upper()
 		flight = trigger.match.group('flight')
-
+		
 		# validate the carrier code
 		if not code in self.Airlines:
 			replytext = "%s is not a valid airline carrier code" % code
 			self.sendReply(trigger, replytext)
 			return
-
+		
 		# validate that they gave us a number to search for, and not "gnashuus"
 		try:
 			flight_num = int(flight)
@@ -153,7 +155,7 @@ class Airline(Plugin):
 			replytext = "\02'%s'\02 is not a valid number" % flight
 			self.sendReply(trigger, replytext)
 			return
-
+		
 		# did they supply a date? If so, use that for the query, otherwise use
 		# today's date
 		try:
@@ -176,88 +178,47 @@ class Airline(Plugin):
 		self.urlRequest(trigger, url)
 	
 	# -----------------------------------------------------------------------
-	
 	# Travelocity has replied
 	def _message_REPLY_URL(self, message):
 		trigger, page_text = message.data
 		
-		# this ugly parsing is ripped right from the pinky java.
-		# .. much like the rest of this plugin, really
-		#
-		# There is most likely a better way to do this, but this "works" so
-		# I'll leave it for now
-		s = StringIO(page_text)
-		found = 0
-		line = s.readline()
-		while line:
-			if line.lower() == "<td valign=top align=right nowrap><b>city:</b>&nbsp;</td>\n":
-				line = s.readline()
-				source = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				dest = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				source_sched = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				dest_sched = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				source_act = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				dest_act = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				source_gate = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				dest_gate = self.__rip(line)
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				line = s.readline()
-				dest_bags = self.__rip(line)
-
-				found = 1
-
+		if trigger.name == AIRLINE_FLIGHT:
+			# No results
+			if page_text.find('No match found') >= 0:
+				replytext = 'Unable to find flight information'
+				self.sendReply(trigger, replytext)
+			
+			else:
+				# Find the chunk of data we're interested in
+				chunk = FindChunk(page_text, '<table name=flight_info', '</table')
+				if chunk is None:
+					self.putlog(LOG_WARNING, 'Flight page parsing failed: unable to find data')
+					self.sendReply(trigger, 'Failed to parse page properly')
+					return
+				lines = StripHTML(chunk)
+				
+				# Get our data!
+				source = lines[3]
+				dest = lines[4]
+				
+				source_sched = lines[6]
+				dest_sched = lines[7]
+				
+				source_act = lines[9]
+				dest_act = lines[10]
+				
+				source_gate = lines[12]
+				dest_gate = lines[13]
+				
+				dest_bags = lines[15]
+				
 				# Should this plugin always reply to successful queries in
 				# a /msg? If so, the hack below is required
 				#trigger.event.IRCType = IRCT_MSG
-
+				
 				# Build our reply string, and send it back to IRC!
 				replytext = "\02Departure\02: %s - %s (%s) Gate: %s" % (source, source_sched, source_act, source_gate)
 				replytext += " --> \02Arrival\02: %s - %s (%s) Gate: %s Baggage: %s" % (dest, dest_sched, dest_act, dest_gate, dest_bags)
 				self.sendReply(trigger, replytext)
 
-			# Keep looking for results
-			line = s.readline()
-		
-		
-		# end of while loop
-		
-		if not found:
-			replytext = "Error finding flight"
-			self.sendReply(trigger, replytext)
-
-		s.close()
-	
-	# -----------------------------------------------------------------------
-
-	# Rip all the HTML out of the lines that we get from travelocity
-	def __rip(self, text):
-		text = re.sub("<.+?>", "", text)
-		text = re.sub("&nbsp;", " ", text)
-		return text[:-1]
-
-	# -----------------------------------------------------------------------
-	
+# ---------------------------------------------------------------------------
