70ba6d2407e4f4d245df8df20a88bcc969090b1e
Freddie <freddie@madcowdisease.org>
2004-07-08 22:00:38 +0000

    Move the Message class into it's own file
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@2951 35f662cf-58f2-0310-8820-d172d141b225

--- classes/Common.py
+++ classes/Common.py
@@ -12,51 +12,6 @@ import time
 import types
 import urllib
 
-from classes.Constants import REPLY_URL
-
-# ---------------------------------------------------------------------------
-# Generic Message object that gets passed around the queues.
-# ---------------------------------------------------------------------------
-class Message:
-	def __init__(self, source, targets, ident, data):
-		self.source = source
-		self.ident = ident
-		self.data = data
-		
-		
-		# Do some type checking, since people will feed us all sorts of types
-		# of targets.
-		t = type(targets)
-		
-		if t == types.NoneType:
-			self.targets = []
-			self.targetstring = 'ALL'
-		
-		elif t in (types.ListType, types.TupleType):
-			self.targets = list(targets)
-			if targets:
-				self.targetstring = ', '.join(targets)
-			else:
-				self.targetstring = 'ALL'
-		
-		elif t == types.StringType:
-			self.targets = [targets]
-			self.targetstring = targets
-		
-		else:
-			print 'WTF? Invalid targets type: %s' % t
-	
-	# Return a printable string with info about ourself, including
-	# how long it's been since we were sent.
-	def __str__(self):
-		data = repr(self.data)
-		#if len(data) >= 100:
-		#	data = '<data omitted>'
-		if self.ident == REPLY_URL:
-			data = '<data omitted>'
-		
-		return '%s --> %s: (%s) %s' % (self.source, self.targetstring, self.ident, data)
-
 # ---------------------------------------------------------------------------
 # Sort out the wacky timezone into something we can use
 def GetTZ():
