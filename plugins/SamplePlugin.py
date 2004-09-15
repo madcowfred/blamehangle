ee1979af0a005289f06c59d9d86b139523827edb
Freddie <freddie@madcowdisease.org>
2004-09-15 19:08:43 +0000

    Fix the weird naming, oops
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@3145 35f662cf-58f2-0310-8820-d172d141b225

--- plugins/SamplePlugin.py
+++ plugins/SamplePlugin.py
@@ -27,7 +27,7 @@ class SamplePlugin(Plugin):
 	# the plugin is started (hence the self.rehash() call above).
 	def rehash(self):
 		# Load our config into a useful dictionary
-		self.Options = self.SetupOptions('Sample')
+		self.Options = self.SetupOptions('SamplePlugin')
 	
 	# -----------------------------------------------------------------------
 	# A plugin can define the run_once method if it has anything that needs
