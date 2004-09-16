de89519b751294fac28e83adb0dd363fe9ce2a74
Freddie <freddie@madcowdisease.org>
2004-09-16 15:23:49 +0000

    We'll probably need this thing too, duh.
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@3148 35f662cf-58f2-0310-8820-d172d141b225

---
+++ classes/OptionsDict.py
@@ -0,0 +1,13 @@
+# ---------------------------------------------------------------------------
+# $Id$
+# ---------------------------------------------------------------------------
+
+"Simple class to add some helpful functions to our options dictionaries."
+
+class OptionsDict(dict):
+	def get_net(self, option, trigger, chan=None):
+		network = trigger.conn.options['name'].lower()
+		if chan is None:
+			return self[option].get(network, None)
+		else:
+			return self[option].get(network, {}).get(chan.lower(), None)
