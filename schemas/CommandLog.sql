fb409dec0f7ac30912e938922fa0b0453b0b4201
Freddie <freddie@madcowdisease.org>
2004-12-24 04:40:34 +0000

    Update the schema to stop giving errors for cut off commands.
    git-svn-id: svn://svn.madcowdisease.org/mcd/trunk/blamehangle@3493 35f662cf-58f2-0310-8820-d172d141b225

--- schemas/CommandLog.sql
+++ schemas/CommandLog.sql
@@ -2,9 +2,9 @@ CREATE TABLE commandlog (
 	ctime decimal(12, 2) NOT NULL,
 	irctype character varying(16) NOT NULL,
 	network character varying(16) NOT NULL,
-	channel character varying(32),
-	user_nick character varying(64),
+	channel character varying(64),
+	user_nick character varying(32),
 	user_host character varying(128),
-	command character varying(128),
+	command text,
 	PRIMARY KEY (ctime)
 );
