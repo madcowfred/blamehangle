f14e161fcb3402d4eba3220e652980ce630f58d5
Freddie <freddie@madcowdisease.org>
2005-07-06 00:35:16 +0000

    URLs can be longer than 255 characters!
    git-svn-id: svn://svn.madcowdisease.org/mcd/blamehangle/trunk@3779 35f662cf-58f2-0310-8820-d172d141b225

--- schemas/TorrentScraper.sql
+++ schemas/TorrentScraper.sql
@@ -1,6 +1,6 @@
 CREATE TABLE torrents (
 	scrape_time integer NOT NULL,
-	url character varying(255) NOT NULL PRIMARY KEY,
+	url text NOT NULL PRIMARY KEY,
 	filename character varying(255),
 	filesize bigint
 );
