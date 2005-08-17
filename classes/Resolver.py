7b5b258330b9556a7a78d7257bbcce5316ff7f8d
Freddie <freddie@madcowdisease.org>
2005-08-17 05:43:41 +0000

    Few manglings. Remove poor old threading code.
    git-svn-id: svn://svn.madcowdisease.org/mcd/blamehangle/trunk@3808 35f662cf-58f2-0310-8820-d172d141b225

--- classes/Resolver.py
+++ classes/Resolver.py
@@ -35,15 +35,9 @@ interpreter run in 2.3+, yay.
 
 import asyncore
 import os
+import re
 import select
 import sys
-
-import re
-import socket
-import time
-
-from Queue import Empty, Queue
-from threading import Thread
 
 from classes.Children import Child
 from classes.Constants import *
@@ -60,11 +54,9 @@ class Resolver(Child):
 		self.DNSCache = self.loadPickle('.resolver_cache')
 		if self.DNSCache is None:
 			self.DNSCache = SimpleCacheDict(1)
-		self.Threads = []
 		
 		self.Active_Requests = 0
 		self.__Requests = []
-		#self.__Requests = Queue(0)
 		
 		self.rehash()
 	
@@ -80,42 +72,10 @@ class Resolver(Child):
 		self.DNSCache.expire()
 		self.savePickle('.resolver_cache', self.DNSCache)
 	
-	# We start our threads here to allow useful early shutdown
-	#def run_once(self):
-	#	self.__Start_Threads()
-	
 	def run_sometimes(self, currtime):
-		if self.__Requests and self.Active_Requests < self.Options.get('active_resolvers', 2):
+		while self.__Requests and self.Active_Requests < self.Options.get('active_resolvers', 2):
 			message = self.__Requests.pop(0)
-			print self.Active_Requests, message.data[2]
 			NastyResolver(self, message)
-	
-	# -----------------------------------------------------------------------
-	# Start our threads!
-	def __Start_Threads(self):
-		for i in range(self.Options['resolver_threads']):
-			t = Thread(target=ResolverThread, args=(self, i))
-			t.setDaemon(1)
-			t.setName('Resolver %d' % i)
-			self.Threads.append([t, 0])
-			t.start()
-			
-			tolog = 'Started DNS thread: %s' % t.getName()
-			self.putlog(LOG_DEBUG, tolog)
-	
-	# Stop our threads!
-	def __Stop_Threads(self):
-		_sleep = time.sleep
-		
-		# set the flag telling each thread that we would like it to exit
-		for tinfo in self.Threads:
-			tinfo[1] = 1
-		
-		# wait until all threads have exited
-		while [t for t,s in self.Threads if t.isAlive()]:
-			_sleep(0.1)
-		
-		self.putlog(LOG_DEBUG, "All DNS threads halted")
 	
 	# -----------------------------------------------------------------------
 	# Someone wants us to resolve something, woo
@@ -138,44 +98,6 @@ class Resolver(Child):
 				self.sendMessage(message.source, REPLY_DNS, data)
 			else:
 				self.__Requests.append(message)
-
-# ---------------------------------------------------------------------------
-
-def ResolverThread(parent, myindex):
-	_sleep = time.sleep
-	
-	while 1:
-		# see if we have to die now
-		if parent.Threads[myindex][1]:
-			return
-		
-		# see if there's something to look up
-		try:
-			message = parent.Requests.get_nowait()
-		except Empty:
-			_sleep(0.25)
-			continue
-		
-		# well, off we go then
-		trigger, method, host, args = message.data
-		
-		try:
-			results = socket.getaddrinfo(host, None, socket.AF_UNSPEC, socket.SOCK_STREAM)
-		except socket.gaierror:
-			data = [trigger, method, None, args]
-		else:
-			# parse the results a bit here
-			hosts = []
-			for af, socktype, proto, canonname, sa in results:
-				if af == socket.AF_INET:
-					hosts.append((4, sa[0]))
-				elif af == socket.AF_INET6:
-					hosts.append((6, sa[0]))
-			
-			parent.DNSCache[host] = hosts
-			data = [trigger, method, hosts, args]
-		
-		parent.sendMessage(message.source, REPLY_DNS, data)
 
 # ---------------------------------------------------------------------------
 
