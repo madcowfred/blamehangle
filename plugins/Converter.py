b3ef1b64a9eb45286c611c484eb98bb814080847
Freddie <freddie@madcowdisease.org>
2005-08-20 02:14:15 +0000

    Move the conversion tables to a seperate data file. This reduces the complexity of the plugin and allows much easier updating.
    git-svn-id: svn://svn.madcowdisease.org/mcd/blamehangle/trunk@3814 35f662cf-58f2-0310-8820-d172d141b225

--- plugins/Converter.py
+++ plugins/Converter.py
@@ -35,43 +35,42 @@ from classes.Constants import *
 from classes.Plugin import Plugin
 
 # ---------------------------------------------------------------------------
-# Mapping of distance measurements to SI units (meters)
-DISTANCE = {
-	'in': ('inches', 0.0254),
-	'ft': ('feet', 0.3048),
-	'yd': ('yards', 0.9144),
-	'miles': ('miles', 1609.34),
-	'mm': ('millimeters', 0.001),
-	'cm': ('centimeters', 0.01),
-	'm': ('meters', 1),
-	'km': ('kilometers', 1000),
-}
-
-# Mapping of volume measurements to SI units (litres)
-VOLUME = {
-	'ml': ('millilitres', 0.001),
-	'l': ('litres', 1),
-	'pt': ('pints', 0.4731),
-	'qt': ('quarts', 0.9463),
-	'gal': ('gallons', 3.7854),
-}
-
-# Mapping of weight measurements to SI units (grams)
-WEIGHT = {
-	'mg': ('milligrams', 0.001),
-	'cg': ('centigrams', 0.01),
-	'g': ('grams', 1),
-	'kg': ('kilograms', 1000),
-	'oz': ('ounces', 28.3495),
-	'lb': ('pounds', 453.5923),
-	'stone': ('stone', 6350.2931),
-	't': ('tonnes', 1000000),
-}
-
-# ---------------------------------------------------------------------------
 
 class Converter(Plugin):
 	_HelpSection = 'math'
+	
+	def setup(self):
+		self.rehash()
+	
+	def rehash(self):
+		self.__Conversions = {}
+		
+		filename = os.path.join('data', 'conversions')
+		try:
+			conv_file = open(filename, 'r')
+		except IOError:
+			self.putlog(LOG_WARNING, "Can't find data/conversions!")
+		else:
+			section = None
+			for line in conv_file:
+				line = line.strip()
+				# Skip empty/comments
+				if not line or line.startswith('#'):
+					continue
+				
+				# New section
+				if line.startswith('-') and line.endswith('-'):
+					section = line[1:-1]
+					self.__Conversions[section] = {}
+				# Entry!
+				else:
+					# (singular form) (plural form) (ratio to standard unit) [other names]
+					parts = line.split()
+					data = (float(parts[0]), parts[1])
+					for name in parts[1:]:
+						self.__Conversions[section][name] = data
+			
+			conv_file.close()
 	
 	def register(self):
 		self.addTextEvent(
@@ -90,43 +89,19 @@ class Converter(Plugin):
 		
 		if data['from'] == data['to']:
 			replytext = "Don't be an idiot"
-		
+		# Nasty temperature specific hacks
 		elif data['from'] == 'c' and data['to'] == 'f':
 			value = '%.1f' % ((data['amt'] * 9.0 / 5) + 32)
 			replytext = '%s \xb0C == %s \xb0F' % (data['amt'], value)
-		
+		# Nasty temperature specific hacks
 		elif data['from'] == 'f' and data['to'] == 'c':
 			value = '%.1f' % ((data['amt'] - 32) * 5.0 / 9)
 			replytext = '%s \xb0F == %s \xb0C' % (data['amt'], value)
-		
+		# The rest
 		else:
-			for MAP in (DISTANCE, VOLUME, WEIGHT):
-				_from = None
-				_to = None
-				
-				if MAP.has_key(data['from']):
-					_from = MAP[data['from']]
-				else:
-					for key, value in MAP.items():
-						if value[0] == data['from']:
-							_from = value
-							break
-						# Handle non-plurals too
-						elif value[0].endswith('s') and value[0][:-1] == data['from']:
-							_from = value
-							break
-				
-				if MAP.has_key(data['to']):
-					_to = MAP[data['to']]
-				else:
-					for key, value in MAP.items():
-						if value[0] == data['to']:
-							_to = value
-							break
-						# Handle non-plurals too
-						elif value[0].endswith('s') and value[0][:-1] == data['to']:
-							_to = value
-							break
+			for MAP in self.__Conversions.values():
+				_from = MAP.get(data['from'], None)
+				_to = MAP.get(data['to'], None)
 				
 				if _from is not None and _to is not None:
 					break
@@ -134,8 +109,8 @@ class Converter(Plugin):
 			if _from is None or _to is None:
 				replytext = '%(from)s <-> %(to)s is not a valid conversion' % data
 			else:
-				value = '%.2f' % (data['amt'] * _from[1] / _to[1])
-				replytext = '%s %s == %s %s' % (data['amt'], _from[0], value, _to[0])
+				value = '%.2f' % (data['amt'] * _from[0] / _to[0])
+				replytext = '%s %s == %s %s' % (data['amt'], _from[1], value, _to[1])
 		
 		self.sendReply(trigger, replytext)
 
