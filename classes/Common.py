fc0a15535a5a12c38ca0bbac050417ff64a08b86
Freddie <freddie@madcowdisease.org>
2008-10-17 11:59:49 +0000

    Fix SafeFilename()
    git-svn-id: svn://svn.madcowdisease.org/mcd/blamehangle/trunk@4171 35f662cf-58f2-0310-8820-d172d141b225

--- classes/Common.py
+++ classes/Common.py
@@ -305,6 +305,7 @@ def ParseSearchString(column, findme):
 # -----------------------------------------------------------------------
 # Make a "safe" filename that is valid on most systems (Windows at least).
 def SafeFilename(filename):
+	safe_filename = filename
 	for char in ' \\|/:*?<>':
 		safe_filename = safe_filename.replace(char, '_')
 	
